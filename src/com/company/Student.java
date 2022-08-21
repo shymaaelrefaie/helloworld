0d23658aa01f9f02832f433b25f79b59dcbccd85
shymaaelrefaie <shymaa.elrefaie@gmail.com>
2022-08-22 00:20:23 +0200

    Initial commit

---
+++ src/com/company/Student.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Student {
+
+    string firstname;
+
+    string lastname;
+
+    int age;
+
+    public void attendclass()
+    {}
+
+    public void absent()
+    {}
+}
